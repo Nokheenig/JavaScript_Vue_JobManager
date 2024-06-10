d7cb915c6bfac404c94a1e737cf154b50f242324
Yoann <yoann.chamillard@gmail.com>
2024-06-10 18:57:28 +0200

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+// eslint.config.js
+import babelParser from "@babel/eslint-parser";
+
+export default [
+    {
+        languageOptions: {
+            parser: babelParser,
+            parserOptions: {
+                requireConfigFile: false,
+                babelOptions: {
+                  babelrc: false,
+                  configFile: false,
+                  presets: ["@babel/preset-env"]
+                }
+            }
+        }
+    }
+];
